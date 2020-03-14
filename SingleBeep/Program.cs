ea68d9972b198c828ee4dda920e7c08738719bc9
Uğur Öztürk <mail@uozturk.com>
2020-03-14 23:23:11 +0300

    SingleBeep

---
+++ SingleBeep/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace SingleBeep
+{
+    class Program
+    {
+        static async Task PlayBeepAsync(WaveOutEvent outputDevice)
+        {
+            await Task.Run(() =>
+            {
+                while (outputDevice.PlaybackState == PlaybackState.Playing)
+                {
+                }
+            });
+        }
+
+        static async Task RunSoundAsync()
+        {
+            var beeSoundLocalation = "D:\\beep.wav";
+            using (var audioFile = new AudioFileReader(beeSoundLocalation))
+            using (var outputDevice = new WaveOutEvent())
+            {
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+                await PlayBeepAsync(outputDevice);
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                System.Console.Write("Komut giriniz : ");
+                var commands = Console.ReadLine();
+                if (commands == "b")
+                {
+                    await RunSoundAsync();
+                }
+                else if (commands == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
